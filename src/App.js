d1fb49259ebf4a8032d8d79c383233e7be87a4f8
Anik Kumar Das <akd.prince07@gmail.com>
2022-04-07 01:34:32 +0600

    first code done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name:"Anik",
+    job: "Engneer"
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +13,8 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
-        
+        <p>My name is : {person.name +". My job is "+person.job}</p>
+        <h1>This is ANIK kumar Das</h1>
       </header>
     </div>
   );
